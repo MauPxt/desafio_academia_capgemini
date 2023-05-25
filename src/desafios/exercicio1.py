5be14c73f0ecd7044a9b7f1a768bd0f643fbb9a9
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-25 00:15:01 -0300

    refactor: simplify return

--- src/desafios/exercicio1.py
+++ src/desafios/exercicio1.py
@@ -6,17 +6,17 @@ valor de n. A última linha não deve conter nenhum espaço.
 
 
 def escada(tamanho):
-    '''
+    """
     Essa função gera uma "figura" de escada tendo como tamanho o
     parâmetro inserido.
 
     :param tamanho: int: o tamanho desejado da pirâmide
     :return: retorna a "figura" da escada de acordo com o tamanho desejado
-    '''
+    """
 
-    for r in range(tamanho):
-        return "\n".join((" " * (tamanho - r - 1)) + ('*' * (r + 1)) for r in
-                         range(tamanho))
+    return '\n'.join(
+        ' ' * (tamanho - r - 1) + '*' * (r + 1) for r in range(tamanho)
+    )
 
 
 if __name__ == '__main__':
