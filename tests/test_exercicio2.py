60f3459d3d4fd0cb93a668254bcadd3b95bae1c2
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-25 00:15:43 -0300

    tests: fix and add more test cases

--- tests/test_exercicio2.py
+++ tests/test_exercicio2.py
@@ -1,18 +1,36 @@
-import unittest #importando a biblioteca para executar os testes unitários
+import unittest  # importando a biblioteca para executar os testes unitários
 
 from src.desafios.exercicio2 import validar_senha
 
-'''
+"""
 O teste unitário será feito da seguinte maneira:
 Primeiramente, a criação de uma classe que receberá o parametro de teste da biblioteca unittest. 
 Logo após será definida uma função para testar a validade da função do exercício proposto.
-'''
+"""
+
 
 class TestExercicio2(unittest.TestCase):
     def test_validar_senha(self):
         self.assertEqual(validar_senha('Ya3@ff'), 'Senha válida!')
-        self.assertEqual(validar_senha('Ya3'), 'Senha inválida. '
-                                                  'Tente novamente!')
+        self.assertEqual(
+            validar_senha('Ya3'), 'Senha inválida. Tente novamente!'
+        )
+        self.assertEqual(validar_senha(''), 'Senha inválida. Tente novamente!')
+        self.assertEqual(
+            validar_senha('123456'), 'Senha inválida. Tente novamente!'
+        )
+        self.assertEqual(
+            validar_senha('abc123'), 'Senha inválida. Tente novamente!'
+        )
+        self.assertEqual(
+            validar_senha('ABCXYZ'), 'Senha inválida. Tente novamente!'
+        )
+        self.assertEqual(
+            validar_senha('@#$%^&'), 'Senha inválida. Tente novamente!'
+        )
+        self.assertEqual(
+            validar_senha('Ya3@'), 'Senha inválida. Tente novamente!'
+        )
 
 
 if __name__ == '__name__':
