7450159410955f961c1f01ef40fbb7f0466cd106
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-25 00:14:31 -0300

    tests: add more cases and improve readability
    refatorei os testes buscando simplificação;
    formatei utilizando a biblioteca blue.

--- tests/test_exercicio1.py
+++ tests/test_exercicio1.py
@@ -1,25 +1,33 @@
-import unittest #importando a biblioteca para executar os testes unitários
+import unittest  # importando a biblioteca para executar os testes unitários
 
 from src.desafios.exercicio1 import escada
 
-'''
+"""
 O teste unitário será feito da seguinte maneira:
 Primeiramente, a criação de uma classe que receberá o parametro de teste da biblioteca unittest. 
 Logo após será definida uma função para testar a validade da função do exercício proposto.
-'''
-class TestExercicio1(unittest.TestCase):
+"""
+
+
+class TestEscada(unittest.TestCase):
     def test_escada(self):
+        # Testando escada com tamanho 0
+        self.assertEqual(escada(0), '')
+
+        # Testando escada com tamanho 1
         self.assertEqual(escada(1), '*')
 
-        self.assertEqual(escada(2), ' *''\n'
-                                    '**')
+        # Testando escada com tamanho 2
+        escada_2 = ' *\n**'
+        self.assertEqual(escada(2), escada_2)
 
-        self.assertEqual(escada(6),'     *''\n'
-                                    '    **''\n'
-                                    '   ***''\n'
-                                    '  ****''\n'
-                                    ' *****''\n'
-                                    '******')
+        # Testando escada com tamanho 6
+        escada_6 = '     *\n    **\n   ***\n  ****\n *****\n******'
+        self.assertEqual(escada(6), escada_6)
+
+        # Testando escada com tamanho 10
+        escada_10 = '         *\n        **\n       ***\n      ****\n     *****\n    ******\n   *******\n  ********\n *********\n**********'
+        self.assertEqual(escada(10), escada_10)
 
 
 if __name__ == '__name__':
